c883bc7c2f3ba682580c647f6b6127328e8c3f82
JoaoGranja <joaogranja10@hotmail.com>
2020-07-29 18:54:31 +0000

    Increase particle filter size to 100

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -30,7 +30,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 10;  // TODO: Set the number of particles
+  num_particles = 100;  // TODO: Set the number of particles
   
   std::default_random_engine gen;
   std::normal_distribution<double> dist_x(x, std[0]);
@@ -128,13 +128,13 @@ void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted,
          observations[j].id = i;
       }
     }
-    associations.push_back(observations[j].id);
+    associations.push_back(predicted[observations[j].id].id);
     sense_x.push_back(observations[j].x);
     sense_y.push_back(observations[j].y);
   }
   
   // Set assocations for visualization purpose only
-  //SetAssociations(particle, associations, sense_x, sense_y);
+  SetAssociations(particle, associations, sense_x, sense_y);
 
 }
 
